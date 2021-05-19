c4f359b1acb33f8bd156ff91c49937a883ac71da
Nim Jayawardena <nimjay@google.com>
2021-05-19 10:29:25 -0400

    Add new concept: HTTP
    This commit also makes drastic improvements to the CSS concept pages.

--- src/concepts.js
+++ src/concepts.js
@@ -20,10 +20,10 @@ const CONCEPTS = [
     'imgAlt': 'foo',
   },
   {
-    'slug': 'bar',
-    'title': 'bar',
-    'sentence': 'bar',
-    'paragraph': 'bar',
+    'slug': 'http',
+    'title': 'HTTP',
+    'sentence': 'HTTP is the language (application-level protocol) that web browsers and web servers use to communicate.',
+    'paragraph': `When you open example.com, your browser sends a message to example.com's server (computer) that says, "give me your home page". Then example.com's server responds with the contents of the home page. Most of the resources that you view and submit on various websites (such as images, personal data, and form submissions) are loaded in this manner. The format of these messages (requests from the browser and responses from the server) is called "Hypertext Transfer Protocol" (HTTP). HTTP doesn't always involve a web browsers — HTTP is often used by computers talking to other computers.`,
     'imgAlt': 'bar',
   },
 ];
