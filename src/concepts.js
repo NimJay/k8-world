3a3229b3fdc88f6cb5acd74718f612fec71cb47c
Nim Jayawardena <nimjay@google.com>
2021-05-19 12:33:29 -0400

    Add concept: Google Kubernetes Engine (GKE)

--- src/concepts.js
+++ src/concepts.js
@@ -20,6 +20,13 @@ const CONCEPTS = [
     'imgAlt': 'foo',
   },
   {
+    'slug': 'google-kubernetes-engine',
+    'title': 'Google Kubernetes Engine (GKE)',
+    'sentence': `Google Kubernetes Engine (GKE) is a service that lets you run and manage your apps on Google's servers through Kubernetes (K8).`,
+    'paragraph': `If you want your app to serve millions of users, you may need to break your app into a set of smaller apps (microservices) that can run on separate servers. These smaller apps need to be placed inside containers (similar to virtual machines) to ensure that they behave the same across different computers/servers. Thousands of these containers can be deployed and managed through the Kubernetes platform. Basically, GKE uses Kubernetes to help you upload, deploy and manage your containerized apps. Two similar services are Amazon Elastic Kubernetes Service (EKS) and Azure Kubernetes Service (AKS). Learn more about GKE <a href="https://cloud.google.com/kubernetes-engine">here</a>.`,
+    'imgAlt': 'bar',
+  },
+  {
     'slug': 'http',
     'title': 'HTTP',
     'sentence': 'HTTP is the language (application-level protocol) that web browsers and web servers use to communicate.',
